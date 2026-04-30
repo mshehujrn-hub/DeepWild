1e5590313f1fc64122abb9c8c0e180dd709730af
Mubarak Mohammed <mshehujrn@gmail.com>
2026-04-30 13:02:48 +0100

    Added model

--- services/inference.py
+++ services/inference.py
@@ -1,8 +1,28 @@
 import torch
 import torch.nn.functional as F
 from model.model import build_model
+import gdown
+import json
+import os
+
+# Google Drive download
+def download_model():
+    if not os.path.exists("model/neuralnet.pth"):
+        os.makedirs("model", exist_ok=True)
+        print("Downloading model from Google Drive...")
+        gdown.download(
+            id="18bju8k1A8KiCsHGLaySPt0KH-p_uaWLy",  # Google Drive file ID
+            output="model/neuralnet.pth",
+            quiet=False
+        )
+        print("Model downloaded successfully.")
+
+#Load class mapping
+with open("model/class_to_idx.json", "r") as f:
+    class_to_idx = json.load(f)
 
 def load_model():
+    download_model()
     model = build_model()
     model.load_state_dict(
         torch.load("model/neuralnet.pth", map_location="cpu")
